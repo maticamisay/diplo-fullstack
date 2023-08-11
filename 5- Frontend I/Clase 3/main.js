fabe114495e2c46b88b874df21bd492073988716
maticamisay <maticamisay99@hotmail.com>
2023-08-10 21:09:36 -0300

    adding styles and update and create actions

--- 5- Frontend I/Clase 3/main.js
+++ 5- Frontend I/Clase 3/main.js
@@ -1,12 +1,38 @@
 const form = document.querySelector("#form");
 const users = [];
+let isEditing = false;
+let editingIdUser = null;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
   const data = Object.fromEntries(formData);
 
+  if (data.password.length < 6) {
+    alert("La contraseña debe tener al menos 6 caracteres");
+    form.password.classList.add("error-input");
+    form.reset();
+    return;
+  }
+  form.password.classList.remove("error-input");
+  if (isEditing) {
+    // buscar usuario en users
+    const user = users.find((user) => user.id == editingIdUser);
+    // actualizar usuario
+    user.name = data.name;
+    user.password = data.password;
+    user.classUser = data.classUser;
+    form.reset();
+    // cambiar isEditing a false
+    isEditing = false;
+    // cambiar editingIdUser a null
+    editingIdUser = null;
+    renderUsers();
+    return;
+  }
+  data.id = Date.now();
   users.push(data);
+  console.log(users);
   form.reset();
   renderUsers();
 });
@@ -14,10 +40,30 @@ form.addEventListener("submit", (e) => {
 function renderUsers() {
   const usersSection = document.querySelector("#usuarios");
   usersSection.innerHTML = "";
-
   users.forEach((user) => {
+    const div = document.createElement("div");
+    div.setAttribute("id", "usuarios-list");
     const li = document.createElement("li");
-    li.textContent = `Username: ${user.name}, password: ${user.password}`;
-    usersSection.appendChild(li);
+    const button = document.createElement("button");
+
+    li.textContent = `Usuario:${user.name}. 
+    Clase: ${user.classUser}`;
+    button.textContent = "Editar";
+    button.setAttribute("id", `userid-${user.id}`);
+    button.addEventListener("click", (e) => editUser(e));
+    div.appendChild(li);
+    div.appendChild(button);
+    usersSection.appendChild(div);
   });
+}
+
+function editUser(event) {
+  const idAttribute = event.target.id.split("-");
+  const id = idAttribute[1];
+  const user = users.find((user) => user.id == id);
+  form.name.value = user.name;
+  form.password.value = user.password;
+  form.classUser.value = user.classUser;
+  isEditing = true;
+  editingIdUser = id;
 }
